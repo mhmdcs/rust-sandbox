dafad6b4838d01fc13a973824ffc75df7cfc8b0b
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2025-03-11 21:26:41 +0300

    hello world

---
+++ reference/vol1/hello_cargo/target/debug/deps/hello_cargo-37d76c835e351c85.d
@@ -0,0 +1,5 @@
+/Users/mohammedalmekhlafi/Documents/Web Projects/rust-sandbox/reference/vol1/hello_cargo/target/debug/deps/libhello_cargo-37d76c835e351c85.rmeta: src/main.rs
+
+/Users/mohammedalmekhlafi/Documents/Web Projects/rust-sandbox/reference/vol1/hello_cargo/target/debug/deps/hello_cargo-37d76c835e351c85.d: src/main.rs
+
+src/main.rs:
---
+++ reference/vol1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/mohammedalmekhlafi/Documents/Web\ Projects/rust-sandbox/reference/vol1/hello_cargo/target/debug/hello_cargo: /Users/mohammedalmekhlafi/Documents/Web\ Projects/rust-sandbox/reference/vol1/hello_cargo/src/main.rs
