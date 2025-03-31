44333dc6336a5cf090334af869af63dca3d5293f
mhmdcs <37247525+mhmdcs@users.noreply.github.com>
2025-03-31 21:26:56 +0300

    add whiles and loops

---
+++ reference/vol3/for_loops/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let a = [5, 7, 2, 5, 9];
+
+    for element in a {
+        println!("element is {element}");
+    }
+}
---
+++ reference/vol3/while_loop/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut count = 3;
+
+    while count != 0 {
+        println!("Number is {count}");
+        count -= 1;
+    }
+
+    println!("Stopped!");
+}
